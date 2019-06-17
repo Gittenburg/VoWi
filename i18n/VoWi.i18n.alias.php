f7bedeee42035d843edfdaab881a972cea8602f6
Gittenburg <47354597+Gittenburg@users.noreply.github.com>
2019-06-17 23:05:45 +0200

    remove no longer needed Special:Attach aliases

--- i18n/VoWi.i18n.alias.php
+++ i18n/VoWi.i18n.alias.php
@@ -1,12 +1,10 @@
 <?php
 $specialPageAliases = [];
 $specialPageAliases['en'] = [
-	'FlexiblePrefix' => ['FlexiblePrefix', 'SimilarNamedArticles', 'Similar named articles'],
-	'Attach' => ['Attach', 'AddResource', 'Add Resource']
+	'FlexiblePrefix' => ['FlexiblePrefix', 'SimilarNamedArticles', 'Similar named articles']
 ];
 $specialPageAliases['de'] = [
 	'AddLVA' => ['LVA-Seite anlegen', 'LVA-Beschreibung hinzufügen'],
 	'FlexiblePrefix' => ['Flexibler Präfix', 'ÄhnlichBenannteSeiten', 'Ähnlich benannte Seiten'],
-	'Attach' => ['Anhängen', 'Materialien hinzufügen'],
 	'Resources' => ['Materialien']
 ];
