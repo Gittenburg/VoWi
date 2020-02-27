dae3635d9c9238bb0dee09bdfa95e36a8d6dedce
Gittenburg <gittenburg@fsinf.at>
2020-02-27 16:11:40 +0100

    fix YYYY-MM-DD sorting (suggested by Lukas)

--- includes/VoWiHooks.php
+++ includes/VoWiHooks.php
@@ -108,7 +108,7 @@ $args
 				unset($links[$key]);
 			}
 		uksort($links, function ($a, $b){
-			$pattern = '/(.+)(\d{4})(.+)/';
+			$pattern = '/(.+)(\d{4}(-\d{2}-\d{2})?)(.*)/';
 			if (preg_match($pattern, $a, $match_a))
 				if (preg_match($pattern, $b, $match_b) && $match_a[1] == $match_b[1])
 					if ($match_a[2] != $match_b[2])
